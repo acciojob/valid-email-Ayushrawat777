b2271d35b48610a27b2f046d057f8d99292ea5ba
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-03-30 20:18:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 function validEmail(str) {
-  //your JS code here.
-}
+var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
+  if (str.value.match(validRegex)) {
+
+    alert("Valid email address!");
+
+    document.form1.text1.focus();
+
+    return true;
+
+  } else {
+
+    alert("Invalid email address!");
+
+    document.form1.text1.focus();
+
+    return false;
+
+  }
+}
 // Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+/* const str = prompt("Enter an email address.");
+alert(validEmail(str)); */
